81c118e6e999178032a638652df91d19f5e42058
Shauna Porter <shauna.porter@ucdconnect.ie>
2018-03-29 09:55:37 +0100

    Updating scraper to run and sleep

--- rideWithMe/main.py
+++ rideWithMe/main.py
@@ -7,7 +7,7 @@ import requests
 import json
 import mysql.connector
 from mysql.connector import errorcode
-
+import time
 
 api_key = '9057d925a716282b0a6b224cc84b50a713c22a7e'
 api_url="https://api.jcdecaux.com/vls/v1/stations"
@@ -44,43 +44,54 @@ TABLES['dynamic_data'] = (
     "  `last_update` bigint(20) NOT NULL,"
     "  PRIMARY KEY (`number`,`last_update`)"
     ") ENGINE=InnoDB")
+def run():
+    while True:
+        try:
+            cnx=mysql.connector.connect(user='dbikes', password='dublinbikes', host='dbikes.c8m1rhzxgoap.us-east-2.rds.amazonaws.com', database='dbikes', )
+            cursor=cnx.cursor()
+            
+            for name, ddl in TABLES.items():
+                try:
+                    print("Creating table {}: ".format(name), end='')
+                    cursor.execute(ddl)
+                except mysql.connector.Error as err:
+                    if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                        print("already exists.")
+                    else:
+                        print(err.msg)
+                else:
+                    print("OK")
+            
+            add_static_data=("INSERT INTO static_data "
+                            "(number, contract_name, name, address, lat, lng, banking, bonus)"
+                            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+            
+            add_dynamic_data=("INSERT INTO dynamic_data "
+                              "(number, status, bike_stands, available_bike_stands, available_bikes,last_update)"
+                              "VALUES(%s,%s,%s,%s,%s,%s)")
+            
+            cursor.execute("SELECT `number` from `static_data`")
+            ids = [row[0] for row in cursor.fetchall()]
+            cursor.execute("SELECT `number`, `last_update` from `dynamic_data`")
+            d_ids = [row for row in cursor.fetchall()]
+            
+            for stop in json_data:
+                if stop['number'] not in ids:
+                    values=(stop['number'], stop['contract_name'], stop['name'], stop['address'], stop['position']['lat'], stop['position']['lng'], stop['banking'], stop['bonus'])
+                    cursor.execute(add_static_data, values)
+                if (stop['number'], stop['last_update']) not in d_ids:
+                    dvalues=(stop['number'],stop['status'],stop['bike_stands'],stop['available_bike_stands'],stop['available_bikes'],stop['last_update'])
+                    cursor.execute(add_dynamic_data, dvalues)
+            
+            cnx.commit()
+            cursor.close()
+            cnx.close()
+            
+            time.sleep(5*60)
 
-cnx=mysql.connector.connect(user='dbikes', password='dublinbikes', host='dbikes.c8m1rhzxgoap.us-east-2.rds.amazonaws.com', database='dbikes', )
-cursor=cnx.cursor()
+        except:
+            print("Error connecting to database")
 
-for name, ddl in TABLES.items():
-    try:
-        print("Creating table {}: ".format(name), end='')
-        cursor.execute(ddl)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-            print("already exists.")
-        else:
-            print(err.msg)
-    else:
-        print("OK")
+    return
 
-add_static_data=("INSERT INTO static_data "
-                "(number, contract_name, name, address, lat, lng, banking, bonus)"
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
-
-add_dynamic_data=("INSERT INTO dynamic_data "
-                  "(number, status, bike_stands, available_bike_stands, available_bikes,last_update)"
-                  "VALUES(%s,%s,%s,%s,%s,%s)")
-
-cursor.execute("SELECT `number` from `static_data`")
-ids = [row[0] for row in cursor.fetchall()]
-cursor.execute("SELECT `number`, `last_update` from `dynamic_data`")
-d_ids = [row for row in cursor.fetchall()]
-
-for stop in json_data:
-    if stop['number'] not in ids:
-        values=(stop['number'], stop['contract_name'], stop['name'], stop['address'], stop['position']['lat'], stop['position']['lng'], stop['banking'], stop['bonus'])
-        cursor.execute(add_static_data, values)
-    if (stop['number'], stop['last_update']) not in d_ids:
-        dvalues=(stop['number'],stop['status'],stop['bike_stands'],stop['available_bike_stands'],stop['available_bikes'],stop['last_update'])
-        cursor.execute(add_dynamic_data, dvalues)
-
-cnx.commit()
-cursor.close()
-cnx.close()
+run()

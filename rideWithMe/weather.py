575a292655e3913b34bd7fd1211f9d3aa54e6192
Eoin Le Masney <eoin.lemasney@ucdconnect.ie>
2018-04-05 10:18:52 +0100

    weather scraper finished

--- rideWithMe/weather.py
+++ rideWithMe/weather.py
@@ -3,6 +3,7 @@ import requests
 import json
 import mysql.connector
 from mysql.connector import errorcode
+import time
 
 
 
@@ -12,7 +13,7 @@ api_url="http://api.openweathermap.org/data/2.5/weather?APPID=52b3fcc8ca94382baa
 response = requests.get(api_url)
 
 # Print the status code of the response.
-print(response)
+#print(response)
 print(response.json())
 json_data=response.json()
 
@@ -25,42 +26,64 @@ TABLES['weather_data1'] = (
     "CREATE TABLE `weather_data1` ("
     "  `id` int(11) NOT NULL,"
     "  `temp` int(11) NOT NULL,"
-    "  `humidity` int(11) NOT NULL,"
-    "  `clouds` int(11) NOT NULL,"
-    "  `wind` int(11) NOT NULL,"
+    "  `main` varchar(16) NOT NULL,"    
+    "  `description` varchar(16) NOT NULL,"
+    "  `clouds` float(11) NOT NULL,"
+    "  `humidity` float(11) NOT NULL,"
+    "  `wind` float(11) NOT NULL,"
+    "  `icon` varchar(16) NOT NULL,"
     "  PRIMARY KEY (`id`)"
     
     ")ENGINE=InnoDB")
 
+def run():
+    while True:
+        try:
+            cnx=mysql.connector.connect(user='dbikes', password='dublinbikes', host='dbikes.c8m1rhzxgoap.us-east-2.rds.amazonaws.com', database='dbikes', )
 
-cnx=mysql.connector.connect(user='dbikes', password='dublinbikes', host='dbikes.c8m1rhzxgoap.us-east-2.rds.amazonaws.com', database='dbikes', )
+            cursor=cnx.cursor()
+            for name, ddl in TABLES.items():
+                try:
+                    print("Creating table {}: ".format(name), end='')
+                    cursor.execute(ddl)
+                except mysql.connector.Error as err:
+                    if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                        print("already exists.")
+                    else:
+                        print(err.msg)
+                else:
+                    print("OK")
 
-cursor=cnx.cursor()
-for name, ddl in TABLES.items():
-    try:
-        print("Creating table {}: ".format(name), end='')
-        cursor.execute(ddl)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-            print("already exists.")
-        else:
-            print(err.msg)
-    else:
-        print("OK")
+            insert_weather_data1 = ( "INSERT INTO weather_data1 "
+                                    "(id, temp, main, description, clouds, humidity, wind, icon)"
+                                   "VALUES(%s, %s, %s, %s, %s, %s, %s, %s)")
 
-insert_weather_data1 = ( "INSERT INTO weather_data1 "
-                        "(id, temp, humidity, clouds, wind)"
-                       "VALUES(%s,%s,%s,%s,%s,%s)")
 
-cursor.execute("SELECT `id` from `weather_data1`")
-ids = [row[0] for row in cursor.fetchall()]
-print("fetching weather data")
 
-for day in json_data:
-        values=(day['id'], day['temp'], day['name'], day['humidity'], day['clouds'], day['wind'])
-cursor.execute(add_weather_data1, values)
+            cursor.execute("SELECT `id` from `weather_data1`")
+            entries = [row[0] for row in cursor.fetchall()]
+            print('entries are', entries)
+            info = json_data['weather'][0]
+            temp = json_data['main']['temp']
+            wind = json_data['wind']
+            clouds = json_data['clouds']
+            humidity = json_data['main']['humidity']
+            id9 = info['id']
+            print('id9', id9)         
+            
+            if id9 not in entries:
+                data_weather1 = (id9, temp , info['main'], info['description'], clouds['all'], humidity, wind['speed'], info['icon']) 
+                cursor.execute(insert_weather_data1, data_weather1)
 
-cnx.commit()
-cursor.close()
-cnx.close()
+            cnx.commit()
+            cursor.close()
+            cnx.close()
 
+            time.sleep(5*60)
+
+
+        except:
+            print("Error connecting to database")
+    return
+
+run()

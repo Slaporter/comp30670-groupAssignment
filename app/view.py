58942c47f2c91cce54d3efaabd26159b02cee6ab
Shauna Porter <shauna.porter@ucdconnect.ie>
2018-04-19 16:39:12 +0100

    predictions in charts.html page for now

--- app/view.py
+++ app/view.py
@@ -3,9 +3,17 @@ Created on 14 Mar 2018
 
 @author: Slaporter
 '''
-from flask import render_template
+from flask import render_template, request
 from app import app
+import sys
 import mysql.connector
+import string
+import pandas as pd
+import pickle
+from sklearn.ensemble import RandomForestRegressor
+import scipy
+from sklearn.externals import joblib
+
 
 @app.route('/')
 def index():
@@ -26,14 +34,32 @@ def weather():
 
 @app.route('/charts')
 def charts():
+    returnDict={}
+    path=request.url
+    if "day" in path:
+        day= request.args.get('day')
+        time= request.args.get('time')
+        stopnumber = request.args.get('stopnumber')
+        temp=request.args.get('temp')
+    
+        clf = pickle.load(open('models/stop'+str(int(stopnumber))+'.pkl', 'rb'))
+        returnDict['prediction']=int(clf.predict(pd.DataFrame({'hour':[time], 'weekday':[day], 'visibility':[90],'windspeed':[28],'temp':[temp],'humidity':[0.56]}))[0])
+
     con=mysql.connector.connect(user='dbikes', password='dublinbikes', host='dbikes.c8m1rhzxgoap.us-east-2.rds.amazonaws.com', database='dbikes', )
     cursor=con.cursor()
-    cursor.execute("SELECT `lat`, `lng`, `name`, `status`, `available_bike_stands`, `available_bikes`, `last_update` from `current_data`")
+    cursor.execute("SELECT `lat`, `lng`, `name`, `number`, `status`, `available_bike_stands`, `available_bikes`, `last_update` from `current_data`")
     infos=cursor.fetchall()
-    returnDict = {}
     returnDict['infos']=infos
     return render_template("charts.html",**returnDict)
 
+# @app.route('/charts?day')
+# def return_prediction():
+#     returnDict={}
+#     day = request.args.get('day')
+#     time = request.args.get('time')
+#     stop_number = request.args.get('stopnumber')
+#     return render_template("charts.html", **{'hi':1})
+    
 @app.route('/directions')
 def directions():
     con=mysql.connector.connect(user='dbikes', password='dublinbikes', host='dbikes.c8m1rhzxgoap.us-east-2.rds.amazonaws.com', database='dbikes', )
